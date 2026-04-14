a37f34125cca7fe0d868b74815afcdd584e6ae41
Racheli <r0583226902@gmail.com>
2026-04-14 12:34:03 +0300

    number 2 commit

---
+++ server/src/models/Book.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    publishedYear: {
+      type: Number,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
---
+++ server/src/models/Borrow.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const borrowSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    borrowDate: {
+      type: Date,
+      default: Date.now,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Borrow = mongoose.model("Borrow", borrowSchema);
+
+export default Borrow;
